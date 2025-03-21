7f82db55d441d7cdb6f3c7b1bcb912f3ca80c801
Rafael jerson pena <rafa.jefer@gmail.com>
2025-03-21 18:07:15 -0300

    feat: criando o lista e adicionar nova tarefa

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,64 @@
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { v4 as uuidv4 } from 'uuid'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: 'Estudar Programação',
+      description: 'Estudar programação para ficar craque',
+      isCompleted: false
+    },
+    {
+      id: 2,
+      title: 'Estudar Inglês',
+      description: 'Estudar inglês para ficar fluente',
+      isCompleted: false
+    },
+    {
+      id: 3,
+      title: 'Estudar, Estudar, Estudar e Estudar',
+      description: 'Estudar para ficar craque',
+      isCompleted: false
+    }
+  ])
+
+  const onTaskClick = (id) => {
+    const newTasks = tasks.map(task => {
+      if (task.id === id) {
+        return { ...task, isCompleted: !task.isCompleted }
+      }
+      return task
+    })
+
+    setTasks(newTasks)
+  }
+
+  const onDeleteTaskClick = (id) => {
+    const newTasks = tasks.filter(task => task.id !== id)
+    setTasks(newTasks)
+  }
+
+  const onAddTaskClick = (title, description) => {
+    const newTask = {
+      id: uuidv4(),
+      title,
+      description,
+      isCompleted: false
+    }
+    setTasks([...tasks, newTask])
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px] space-y-4">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">Gerenciador de Tarefas</h1>
+        <AddTask onAddTaskClick={onAddTaskClick} />
+        <Tasks tasks={tasks} onTaskClick={onTaskClick} onDeleteTaskClick={onDeleteTaskClick} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
---
+++ src/components/AddTask.jsx
@@ -0,0 +1,51 @@
+
+import { useState } from 'react'
+
+function AddTask ({ onAddTaskClick }) {
+    const [title, setTitle] = useState('')
+    const [description, setDescription] = useState('')
+
+    const onSubmitClick = (title, description) => {
+        title = title.trim()
+        description = description.trim()
+        if (!title) {
+            alert('Digite o título da tarefa')
+            return
+        }
+
+        if (!description) {
+            alert('Digite a descrição da tarefa')
+            return
+        }
+
+        onAddTaskClick(title, description)
+        setTitle('')
+        setDescription('')
+    }
+
+    return (
+      <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow-md flex flex-col">
+        <input
+            type="text"
+            placeholder="Digite o título da tarefa"
+            className="w-full p-2 border border-gray-300 outline-slate-400 rounded-md px-4 py-2"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+        />
+        <textarea
+            placeholder="Digite a descrição da tarefa"
+            className="w-full p-2 border border-gray-300 outline-slate-400 rounded-md px-4 py-2"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+        ></textarea>
+        <button
+            className="bg-slate-500 hover:bg-slate-600 text-white px-4 py-2 rounded-md font-medium cursor-pointer"
+            onClick={() => onSubmitClick(title, description)}
+        >
+            Adicionar
+        </button>
+      </div>
+    )
+}
+
+export default AddTask
